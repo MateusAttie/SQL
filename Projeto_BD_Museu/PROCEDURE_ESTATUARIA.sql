28d177920916f214bd6bcae0c46367bf6e364e67
Mateus Attie <36279842+MateusAttie@users.noreply.github.com>
2018-02-08 20:09:53 -0300

    Add files via upload

---
+++ Projeto_BD_Museu/PROCEDURE_ESTATUARIA.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE ADD_ESTATUARIA(
+  cod_est ESTATUARIA.COD_ESTATUARIA%type,
+  material_est estatuaria.material%type,
+  altura_est estatuaria.altura%type,
+  peso_est estatuaria.peso%type,
+  estilo_est estatuaria.estilo%type)
+  IS
+  
+  BEGIN
+  INSERT INTO ESTATUARIA
+  VALUES(cod_est,material_est,altura_est,peso_est,estilo_est);
+  END;
+  /
+  
+  
+  EXEC ADD_ESTATUARIA(1,'papel sulfite', 0.00005, 0.00001,'cachorro');
+  DESC ESTATUARIA;
+  
+  SELECT * FROM ESTATUARIA;
